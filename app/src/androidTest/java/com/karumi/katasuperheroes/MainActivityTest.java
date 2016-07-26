43fefed0c25a145792edfbb0ebb303cbd8b24e72
David González <malmstein@gmail.com>
2016-07-26 22:12:57 +0100

    adding custom idling resource

--- app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
+++ app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
@@ -17,6 +17,8 @@
 package com.karumi.katasuperheroes;
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
@@ -24,21 +26,28 @@ import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
+
 import com.karumi.katasuperheroes.di.MainComponent;
 import com.karumi.katasuperheroes.di.MainModule;
+import com.karumi.katasuperheroes.idlingresource.RecyclerViewWithContentIdlingResource;
 import com.karumi.katasuperheroes.model.SuperHero;
 import com.karumi.katasuperheroes.model.SuperHeroesRepository;
 import com.karumi.katasuperheroes.recyclerview.RecyclerViewInteraction;
 import com.karumi.katasuperheroes.ui.view.MainActivity;
 import com.karumi.katasuperheroes.ui.view.SuperHeroDetailActivity;
-import it.cosenonjaviste.daggermock.DaggerMockRule;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import it.cosenonjaviste.daggermock.DaggerMockRule;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -56,149 +65,182 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.allOf;
 import static org.mockito.Mockito.when;
 
-@RunWith(AndroidJUnit4.class) @LargeTest public class MainActivityTest {
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
 
-  private static final int ANY_NUMBER_OF_SUPER_HEROES = 10;
+    private static final int ANY_NUMBER_OF_SUPER_HEROES = 10;
 
-  @Rule public DaggerMockRule<MainComponent> daggerRule =
-      new DaggerMockRule<>(MainComponent.class, new MainModule()).set(
-          new DaggerMockRule.ComponentSetter<MainComponent>() {
-            @Override public void setComponent(MainComponent component) {
-              SuperHeroesApplication app =
-                  (SuperHeroesApplication) InstrumentationRegistry.getInstrumentation()
-                      .getTargetContext()
-                      .getApplicationContext();
-              app.setComponent(component);
-            }
-          });
+    private IdlingResource idlingResource;
 
-  @Rule public IntentsTestRule<MainActivity> activityRule =
-      new IntentsTestRule<>(MainActivity.class, true, false);
+    @Rule
+    public DaggerMockRule<MainComponent> daggerRule =
+            new DaggerMockRule<>(MainComponent.class, new MainModule()).set(
+                    new DaggerMockRule.ComponentSetter<MainComponent>() {
+                        @Override
+                        public void setComponent(MainComponent component) {
+                            SuperHeroesApplication app =
+                                    (SuperHeroesApplication) InstrumentationRegistry.getInstrumentation()
+                                            .getTargetContext()
+                                            .getApplicationContext();
+                            app.setComponent(component);
+                        }
+                    });
 
-  @Mock SuperHeroesRepository repository;
-
-  @Test public void showsEmptyCaseIfThereAreNoSuperHeroes() {
-    givenThereAreNoSuperHeroes();
-
-    startActivity();
-
-    onView(withText("¯\\_(ツ)_/¯")).check(matches(isDisplayed()));
-  }
-
-  @Test public void showsSuperHeroesNameIfThereAreSuperHeroes() {
-    List<SuperHero> superHeroes = givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES);
-
-    startActivity();
-
-    RecyclerViewInteraction.<SuperHero>onRecyclerView(withId(R.id.recycler_view))
-        .withItems(superHeroes)
-        .check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>() {
-          @Override public void check(SuperHero superHero, View view, NoMatchingViewException e) {
-            matches(hasDescendant(withText(superHero.getName()))).check(view, e);
-          }
-        });
-  }
-
-  @Test public void showsAvengersBadgeIfASuperHeroIsPartOfTheAvengersTeam() {
-    List<SuperHero> superHeroes = givenThereAreSomeAvengers(ANY_NUMBER_OF_SUPER_HEROES);
-
-    startActivity();
-
-    RecyclerViewInteraction.<SuperHero>onRecyclerView(withId(R.id.recycler_view))
-        .withItems(superHeroes)
-        .check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>() {
-          @Override public void check(SuperHero superHero, View view, NoMatchingViewException e) {
-            matches(hasDescendant(allOf(withId(R.id.iv_avengers_badge),
-                withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))).check(view, e);
-          }
-        });
-  }
-
-  @Test public void doesNotShowAvengersBadgeIfASuperHeroIsNotPartOfTheAvengersTeam() {
-    List<SuperHero> superHeroes = givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES, false);
-
-    startActivity();
-
-    RecyclerViewInteraction.<SuperHero>onRecyclerView(withId(R.id.recycler_view))
-        .withItems(superHeroes)
-        .check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>() {
-          @Override public void check(SuperHero superHero, View view, NoMatchingViewException e) {
-            matches(hasDescendant(allOf(withId(R.id.iv_avengers_badge),
-                withEffectiveVisibility(ViewMatchers.Visibility.GONE)))).check(view, e);
-          }
-        });
-  }
-
-  @Test public void doesNotShowEmptyCaseIfThereAreSuperHeroes() {
-    givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES);
-
-    startActivity();
-
-    onView(withId(R.id.tv_empty_case)).check(matches(not(isDisplayed())));
-  }
-
-  @Test public void doesNotShowLoadingViewOnceSuperHeroesAreShown() {
-    givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES);
-
-    startActivity();
-
-    onView(withId(R.id.progress_bar)).check(matches(not(isDisplayed())));
-  }
-
-  @Test public void opensSuperHeroDetailActivityOnRecyclerViewItemTapped() {
-    List<SuperHero> superHeroes = givenThereAreSomeSuperHeroes();
-    int superHeroIndex = 0;
-    startActivity();
-
-    onView(withId(R.id.recycler_view)).
-        perform(RecyclerViewActions.actionOnItemAtPosition(superHeroIndex, click()));
-
-    SuperHero superHeroSelected = superHeroes.get(superHeroIndex);
-    intended(hasComponent(SuperHeroDetailActivity.class.getCanonicalName()));
-    intended(hasExtra("super_hero_name_key", superHeroSelected.getName()));
-  }
-
-  @Test public void showsTheExactNumberOfSuperHeroes() {
-    givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES);
-
-    startActivity();
-
-    onView(withId(R.id.recycler_view)).check(
-        matches(recyclerViewHasItemCount(ANY_NUMBER_OF_SUPER_HEROES)));
-  }
-
-  private List<SuperHero> givenThereAreSomeAvengers(int numberOfAvengers) {
-    return givenThereAreSomeSuperHeroes(numberOfAvengers, true);
-  }
-
-  private List<SuperHero> givenThereAreSomeSuperHeroes() {
-    return givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES);
-  }
-
-  private List<SuperHero> givenThereAreSomeSuperHeroes(int numberOfSuperHeroes) {
-    return givenThereAreSomeSuperHeroes(numberOfSuperHeroes, false);
-  }
-
-  private List<SuperHero> givenThereAreSomeSuperHeroes(int numberOfSuperHeroes, boolean avengers) {
-    List<SuperHero> superHeroes = new LinkedList<>();
-    for (int i = 0; i < numberOfSuperHeroes; i++) {
-      String superHeroName = "SuperHero - " + i;
-      String superHeroPhoto = "https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg";
-      String superHeroDescription = "Description Super Hero - " + i;
-      SuperHero superHero =
-          new SuperHero(superHeroName, superHeroPhoto, avengers, superHeroDescription);
-      superHeroes.add(superHero);
-      when(repository.getByName(superHeroName)).thenReturn(superHero);
+    @Before
+    public void registerIntentServiceIdlingResource() {
+        idlingResource = new RecyclerViewWithContentIdlingResource(
+                activityRule.getActivity(),
+                R.id.recycler_view,
+                ANY_NUMBER_OF_SUPER_HEROES);
+        Espresso.registerIdlingResources(idlingResource);
     }
-    when(repository.getAll()).thenReturn(superHeroes);
-    return superHeroes;
-  }
 
-  private void givenThereAreNoSuperHeroes() {
-    when(repository.getAll()).thenReturn(Collections.<SuperHero>emptyList());
-  }
+    @After
+    public void unregisterIntentServiceIdlingResource() {
+        Espresso.unregisterIdlingResources(idlingResource);
+    }
 
-  private MainActivity startActivity() {
-    return activityRule.launchActivity(null);
-  }
+    @Rule
+    public IntentsTestRule<MainActivity> activityRule =
+            new IntentsTestRule<>(MainActivity.class, true, false);
+
+    @Mock
+    SuperHeroesRepository repository;
+
+    @Test
+    public void showsEmptyCaseIfThereAreNoSuperHeroes() {
+        givenThereAreNoSuperHeroes();
+
+        startActivity();
+
+        onView(withText("¯\\_(ツ)_/¯")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void showsSuperHeroesNameIfThereAreSuperHeroes() {
+        List<SuperHero> superHeroes = givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES);
+
+        startActivity();
+
+        RecyclerViewInteraction.<SuperHero>onRecyclerView(withId(R.id.recycler_view))
+                .withItems(superHeroes)
+                .check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>() {
+                    @Override
+                    public void check(SuperHero superHero, View view, NoMatchingViewException e) {
+                        matches(hasDescendant(withText(superHero.getName()))).check(view, e);
+                    }
+                });
+    }
+
+    @Test
+    public void showsAvengersBadgeIfASuperHeroIsPartOfTheAvengersTeam() {
+        List<SuperHero> superHeroes = givenThereAreSomeAvengers(ANY_NUMBER_OF_SUPER_HEROES);
+
+        startActivity();
+
+        RecyclerViewInteraction.<SuperHero>onRecyclerView(withId(R.id.recycler_view))
+                .withItems(superHeroes)
+                .check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>() {
+                    @Override
+                    public void check(SuperHero superHero, View view, NoMatchingViewException e) {
+                        matches(hasDescendant(allOf(withId(R.id.iv_avengers_badge),
+                                withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))).check(view, e);
+                    }
+                });
+    }
+
+    @Test
+    public void doesNotShowAvengersBadgeIfASuperHeroIsNotPartOfTheAvengersTeam() {
+        List<SuperHero> superHeroes = givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES, false);
+
+        startActivity();
+
+        RecyclerViewInteraction.<SuperHero>onRecyclerView(withId(R.id.recycler_view))
+                .withItems(superHeroes)
+                .check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>() {
+                    @Override
+                    public void check(SuperHero superHero, View view, NoMatchingViewException e) {
+                        matches(hasDescendant(allOf(withId(R.id.iv_avengers_badge),
+                                withEffectiveVisibility(ViewMatchers.Visibility.GONE)))).check(view, e);
+                    }
+                });
+    }
+
+    @Test
+    public void doesNotShowEmptyCaseIfThereAreSuperHeroes() {
+        givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES);
+
+        startActivity();
+
+        onView(withId(R.id.tv_empty_case)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void doesNotShowLoadingViewOnceSuperHeroesAreShown() {
+        givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES);
+
+        startActivity();
+
+        onView(withId(R.id.progress_bar)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void opensSuperHeroDetailActivityOnRecyclerViewItemTapped() {
+        List<SuperHero> superHeroes = givenThereAreSomeSuperHeroes();
+        int superHeroIndex = 0;
+        startActivity();
+
+        onView(withId(R.id.recycler_view)).
+                perform(RecyclerViewActions.actionOnItemAtPosition(superHeroIndex, click()));
+
+        SuperHero superHeroSelected = superHeroes.get(superHeroIndex);
+        intended(hasComponent(SuperHeroDetailActivity.class.getCanonicalName()));
+        intended(hasExtra("super_hero_name_key", superHeroSelected.getName()));
+    }
+
+    @Test
+    public void showsTheExactNumberOfSuperHeroes() {
+        givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES);
+
+        startActivity();
+
+        onView(withId(R.id.recycler_view)).check(
+                matches(recyclerViewHasItemCount(ANY_NUMBER_OF_SUPER_HEROES)));
+    }
+
+    private List<SuperHero> givenThereAreSomeAvengers(int numberOfAvengers) {
+        return givenThereAreSomeSuperHeroes(numberOfAvengers, true);
+    }
+
+    private List<SuperHero> givenThereAreSomeSuperHeroes() {
+        return givenThereAreSomeSuperHeroes(ANY_NUMBER_OF_SUPER_HEROES);
+    }
+
+    private List<SuperHero> givenThereAreSomeSuperHeroes(int numberOfSuperHeroes) {
+        return givenThereAreSomeSuperHeroes(numberOfSuperHeroes, false);
+    }
+
+    private List<SuperHero> givenThereAreSomeSuperHeroes(int numberOfSuperHeroes, boolean avengers) {
+        List<SuperHero> superHeroes = new LinkedList<>();
+        for (int i = 0; i < numberOfSuperHeroes; i++) {
+            String superHeroName = "SuperHero - " + i;
+            String superHeroPhoto = "https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg";
+            String superHeroDescription = "Description Super Hero - " + i;
+            SuperHero superHero =
+                    new SuperHero(superHeroName, superHeroPhoto, avengers, superHeroDescription);
+            superHeroes.add(superHero);
+            when(repository.getByName(superHeroName)).thenReturn(superHero);
+        }
+        when(repository.getAll()).thenReturn(superHeroes);
+        return superHeroes;
+    }
+
+    private void givenThereAreNoSuperHeroes() {
+        when(repository.getAll()).thenReturn(Collections.<SuperHero>emptyList());
+    }
+
+    private MainActivity startActivity() {
+        return activityRule.launchActivity(null);
+    }
 }
