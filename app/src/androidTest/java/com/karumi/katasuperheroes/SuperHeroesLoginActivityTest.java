d1657be1e56fbaadff6f71fac7dbf20d5812e367
stefana-a-st <stefana-alexia.stelea@sky.uk>
2016-08-15 13:04:30 +0200

    Add tests for the LoginActivity

--- app/src/androidTest/java/com/karumi/katasuperheroes/SuperHeroesLoginActivityTest.java
+++ app/src/androidTest/java/com/karumi/katasuperheroes/SuperHeroesLoginActivityTest.java
@@ -16,29 +16,42 @@
 
 package com.karumi.katasuperheroes;
 
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.intent.rule.IntentsTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
-
 import com.karumi.katasuperheroes.di.MainComponent;
 import com.karumi.katasuperheroes.di.MainModule;
 import com.karumi.katasuperheroes.model.SuperHeroesRepository;
+import com.karumi.katasuperheroes.ui.view.SuperHeroesActivity;
 import com.karumi.katasuperheroes.ui.view.SuperHeroesLoginActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
 
 import it.cosenonjaviste.daggermock.DaggerMockRule;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.VerificationModes.times;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class SuperHeroesLoginActivityTest {
 
-    @Rule
-    public DaggerMockRule<MainComponent> daggerRule =
+    @Rule public DaggerMockRule<MainComponent> daggerRule =
             new DaggerMockRule<>(MainComponent.class, new MainModule()).set(
                     new DaggerMockRule.ComponentSetter<MainComponent>() {
                         @Override
@@ -51,21 +64,50 @@ public class SuperHeroesLoginActivityTest {
                         }
                     });
 
-    @Rule
-    public IntentsTestRule<SuperHeroesLoginActivity> activityRule =
+    @Rule public IntentsTestRule<SuperHeroesLoginActivity> activityRule =
             new IntentsTestRule<>(SuperHeroesLoginActivity.class, true, false);
 
-    @Mock
-    SuperHeroesRepository repository;
+    @Mock SuperHeroesRepository repository;
 
-    @Test
-    public void showsErrorMessageIfWrongCredentials() {
+    @Test public void showsErrorMessageIfWrongCredentials() {
 
+        activityRule.launchActivity(null);
+
+        onView(withId(R.id.login_email))
+                .perform(typeText("notok@google.com"), closeSoftKeyboard());
+
+        onView(withId(R.id.action_login)).perform(click());
+
+        onView(withId(android.support.design.R.id.snackbar_text))
+                .check(matches(isDisplayed()));
     }
 
-    @Test
-    public void openSuperHeroesIfValidCredentials() {
+    @Test public void showsErrorMessageIfWrongCredentialsIntent() {
+        activityRule.launchActivity(null);
 
+        onView(withId(R.id.login_email))
+                .perform(typeText("notok@google.com"), closeSoftKeyboard());
+
+        onView(withId(R.id.action_login)).perform(click());
+
+        // intended(allOf(toPackage("com.karumi.katasuperheroes.superheroes")));
+
+        intended(toPackage("com.karumi.katasuperheroes.ui.superheroesactivity"), times(0));
     }
 
+    @Test public void openSuperHeroesIfValidCredentials() {
+        activityRule.launchActivity(null);
+
+        onView(withId(R.id.login_email))
+                .perform(typeText("ok@google.com"), closeSoftKeyboard());
+
+        onView(withId(R.id.login_password))
+                .perform(typeText("securepassword"), closeSoftKeyboard());
+
+        Mockito.when(repository.login("ok@google.com", "securepassword")).thenReturn(true);
+
+        onView(withId(R.id.action_login)).perform(click());
+
+        intended(hasComponent(SuperHeroesActivity.class.getCanonicalName()));
+    }
 }
